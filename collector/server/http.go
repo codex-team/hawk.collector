2a4ad41cd8d6c868381ff86b3ba9170c965c5401
N0str <ntpcp@yandex.ru>
2019-08-26 00:08:06 +0300

    First prototype

--- collector/server/http.go
+++ collector/server/http.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"log"
 
 	"github.com/valyala/fasthttp"
@@ -20,6 +23,8 @@ func RequestHandler(ctx *fasthttp.RequestCtx) {
 	switch string(ctx.Path()) {
 	case "/":
 		catcherHTTPHandler(ctx)
+	case "/sourcemap":
+		sourcemapUploadHandler(ctx)
 	case "/ws", "/ws/":
 		catcherWebsocketsHandler(ctx)
 	default:
@@ -30,6 +35,22 @@ func RequestHandler(ctx *fasthttp.RequestCtx) {
 
 // catcherHTTPHandler processes HTTP requests
 func catcherHTTPHandler(ctx *fasthttp.RequestCtx) {
-	log.Printf("%s request from %s", ctx.Method(), ctx.RemoteIP())
+	log.Printf("%s catcherHTTPHandler request from %s", ctx.Method(), ctx.RemoteIP())
 	SendAnswer(ctx, processMessage(ctx.PostBody()))
+}
+
+func sourcemapUploadHandler(ctx *fasthttp.RequestCtx) {
+	log.Printf("%s sourcemapUploadHandler request from %s", ctx.Method(), ctx.RemoteIP())
+	form, _ := ctx.MultipartForm()
+	for _, v := range form.File {
+		for _, header := range v {
+			fasthttp.SaveMultipartFile(header, fmt.Sprintf("/tmp/%s", header.Filename))
+			f, _ := header.Open()
+			defer f.Close()
+			buf := bytes.NewBuffer(nil)
+			io.Copy(buf, f)
+
+			fmt.Printf("%d", len(buf.Bytes()))
+		}
+	}
 }
