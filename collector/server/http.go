5189588e75c3a8bf06d87bc2dd202800c4b38e02
N0str <ntpcp@yandex.ru>
2019-08-26 00:52:49 +0300

    Add rabbit

--- collector/server/http.go
+++ collector/server/http.go
@@ -1,9 +1,6 @@
 package server
 
 import (
-	"bytes"
-	"fmt"
-	"io"
 	"log"
 
 	"github.com/valyala/fasthttp"
@@ -41,16 +38,17 @@ func catcherHTTPHandler(ctx *fasthttp.RequestCtx) {
 
 func sourcemapUploadHandler(ctx *fasthttp.RequestCtx) {
 	log.Printf("%s sourcemapUploadHandler request from %s", ctx.Method(), ctx.RemoteIP())
-	form, _ := ctx.MultipartForm()
-	for _, v := range form.File {
-		for _, header := range v {
-			fasthttp.SaveMultipartFile(header, fmt.Sprintf("/tmp/%s", header.Filename))
-			f, _ := header.Open()
-			defer f.Close()
-			buf := bytes.NewBuffer(nil)
-			io.Copy(buf, f)
 
-			fmt.Printf("%d", len(buf.Bytes()))
-		}
+	token := ctx.Request.Header.Peek("Authentication")
+	if len(token) == 0 {
+		SendAnswer(ctx, Response{true, "Provide Authentication header", fasthttp.StatusBadRequest})
+	}
+
+	form, err := ctx.MultipartForm()
+	if err != nil {
+		log.Printf("Error: %s", err)
+		SendAnswer(ctx, Response{true, "Multipart form is not provided", fasthttp.StatusBadRequest})
+	} else {
+		SendAnswer(ctx, UploadSourcemap(form, token))
 	}
 }
--- collector/server/sourcemap.go
+++ collector/server/sourcemap.go
@@ -1,5 +1,51 @@
 package server
 
-func UploadSourcemap()  {
-	
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"mime/multipart"
+
+	"github.com/codex-team/hawk.collector/collector/lib"
+	"github.com/valyala/fasthttp"
+)
+
+const sourcemapQueue = "release/javascript"
+
+type SourcemapFile struct {
+	Name    string `json:"name"`
+	Payload []byte `json:"payload"`
+}
+
+type SourcemapMessage struct {
+	Token string          `json:"token"`
+	Files []SourcemapFile `json:"files"`
+}
+
+func UploadSourcemap(form *multipart.Form, token []byte) Response {
+	var files []SourcemapFile
+	for _, v := range form.File {
+		for _, header := range v {
+			f, _ := header.Open()
+			defer f.Close()
+			buf := bytes.NewBuffer(nil)
+			_, err := io.Copy(buf, f)
+			if err != nil {
+				break
+			}
+			files = append(files, SourcemapFile{Name: header.Filename, Payload: buf.Bytes()})
+		}
+	}
+	messageToSend := SourcemapMessage{Token: string(token), Files: files}
+
+	// Marshal JSON to string to send to queue
+	minifiedMessage, err := json.Marshal(messageToSend)
+	if err != nil {
+		log.Printf("JSON compression error: %v", err)
+		return Response{true, "Server error", fasthttp.StatusInternalServerError}
+	}
+
+	messagesQueue <- lib.Message{Payload: minifiedMessage, Route: sourcemapQueue}
+	return Response{false, "OK", fasthttp.StatusOK}
 }
