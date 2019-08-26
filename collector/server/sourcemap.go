75668bb7ad7f6c7fe9f868ded42d62e01a38b909
N0str <ntpcp@yandex.ru>
2019-08-26 22:33:06 +0300

    Add release form value processing

--- collector/server/sourcemap.go
+++ collector/server/sourcemap.go
@@ -19,12 +19,21 @@ type SourcemapFile struct {
 }
 
 type SourcemapMessage struct {
-	Token string          `json:"token"`
-	Files []SourcemapFile `json:"files"`
+	Token   string          `json:"token"`
+	Release string          `json:"release"`
+	Files   []SourcemapFile `json:"files"`
 }
 
 func UploadSourcemap(form *multipart.Form, token []byte) Response {
 	var files []SourcemapFile
+	releaseValues, ok := form.Value["release"]
+	if !ok {
+		return Response{true, "Provide `release` form value", fasthttp.StatusInternalServerError}
+	}
+	if len(releaseValues) != 1 {
+		return Response{true, "Provide single `release` form value", fasthttp.StatusInternalServerError}
+	}
+	release := releaseValues[0]
 	for _, v := range form.File {
 		for _, header := range v {
 			f, _ := header.Open()
@@ -37,7 +46,7 @@ func UploadSourcemap(form *multipart.Form, token []byte) Response {
 			files = append(files, SourcemapFile{Name: header.Filename, Payload: buf.Bytes()})
 		}
 	}
-	messageToSend := SourcemapMessage{Token: string(token), Files: files}
+	messageToSend := SourcemapMessage{Token: string(token), Files: files, Release: release}
 
 	// Marshal JSON to string to send to queue
 	minifiedMessage, err := json.Marshal(messageToSend)
