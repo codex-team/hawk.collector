802947b1a3ebcae1b58eccdf9fe9829ae3d8c962
N0str <ntpcp@yandex.ru>
2019-09-13 23:26:30 +0300

    Fix dockerfile

--- cmd/config.go
+++ cmd/config.go
@@ -3,8 +3,6 @@ package cmd
 type Config struct {
 	BrokerURL                      string `env:"BROKER_URL"`
 	Exchange                       string `env:"EXCHANGE"`
-	Host                           string `env:"HOST"`
-	Port                           int    `env:"PORT"`
 	RetryNumber                    int    `env:"RETRY_NUMBER"`
 	RetryInterval                  uint   `env:"RETRY_INTERVAL"`
 	JwtSecret                      string `env:"JWT_SECRET"`
